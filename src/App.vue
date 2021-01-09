5868b1dcd328f4932aef0438fcf7fe68b1d70cb2
puliv <pverapavez@gmail.com>
2021-01-09 20:43:48 -0300

    ok

--- src/App.vue
+++ src/App.vue
@@ -21,7 +21,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin: 50px;
+  display: flex;
+  justify-content: center;
+}
+body {
+  background-color: #000;
 }
 </style>
--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -4,29 +4,32 @@
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div class="btn operator">÷</div>
-    <div class="btn">7</div>
-    <div class="btn">8</div>
-    <div class="btn">9</div>
-    <div class="btn operator">X</div>
-    <div class="btn">4</div>
-    <div class="btn">5</div>
-    <div class="btn">6</div>
-    <div class="btn operator">-</div>
-    <div class="btn">1</div>
-    <div class="btn">2</div>
-    <div class="btn">3</div>
-    <div class="btn operator">+</div>
-    <div class="btn zero">0</div>
-    <div class="btn">.</div>
-    <div class="btn operator">=</div>
+    <div @click="divide" class="btn operator">÷</div>
+    <div @click="append('7')" class="btn">7</div>
+    <div @click="append('8')" class="btn">8</div>
+    <div @click="append('9')" class="btn">9</div>
+    <div @click="multiply" class="btn operator">X</div>
+    <div @click="append('4')" class="btn">4</div>
+    <div @click="append('5')" class="btn">5</div>
+    <div @click="append('6')" class="btn">6</div>
+    <div @click="minus" class="btn operator">-</div>
+    <div @click="append('1')" class="btn">1</div>
+    <div @click="append('2')" class="btn">2</div>
+    <div @click="append('3')" class="btn">3</div>
+    <div @click="add" class="btn operator">+</div>
+    <div @click="append('0')" class="btn zero">0</div>
+    <div @click="dot" class="btn">.</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      current: "500"
+      previous: null,
+      current: "",
+      operator: null,
+      operatorClicked: false
     };
   },
   methods: {
@@ -34,10 +37,46 @@ export default {
       this.current = "";
     },
     sign() {
-      this.current = this.current.charAt(0) === "-" ? this.current.slice(1) : `-${this.current}`;
+      this.current *= -1;
     },
     percent() {
       this.current = `${parseFloat(this.current) / 100}`;
+    },
+    append(number) {
+      if (this.operatorClicked) {
+        this.current = "";
+        this.operatorClicked = false;
+      }
+      this.current = this.current + number;
+    },
+    dot() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
+      }
+    },
+    setPrevious() {
+      this.previous = this.current;
+      this.operatorClicked = true;
+    },
+    divide() {
+      this.setPrevious();
+      this.operator = (a, b) => a / b;
+    },
+    multiply() {
+      this.setPrevious();
+      this.operator = (a, b) => a * b;
+    },
+    minus() {
+      this.setPrevious();
+      this.operator = (a, b) => a - b;
+    },
+    add() {
+      this.setPrevious();
+      this.operator = (a, b) => a + b;
+    },
+    equal() {
+      this.current = `${this.operator(parseFloat(this.previous), parseFloat(this.current))}`;
+      this.previous = null;
     }
   }
 };
@@ -60,6 +99,7 @@ export default {
 .btn {
   border: 1px solid #333;
   background-color: #eee;
+  cursor: pointer;
 }
 .operator {
   background-color: orange;
