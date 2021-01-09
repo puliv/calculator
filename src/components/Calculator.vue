58a6c4b11905a7e34fcf2f965f19e2965e18ee22
puliv <pverapavez@gmail.com>
2021-01-09 18:26:34 -0300

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="calculator">
+    <div class="display">{{ current || "0" }}</div>
+    <div @click="clear" class="btn">C</div>
+    <div @click="sign" class="btn">+/-</div>
+    <div @click="percent" class="btn">%</div>
+    <div class="btn operator">÷</div>
+    <div class="btn">7</div>
+    <div class="btn">8</div>
+    <div class="btn">9</div>
+    <div class="btn operator">X</div>
+    <div class="btn">4</div>
+    <div class="btn">5</div>
+    <div class="btn">6</div>
+    <div class="btn operator">-</div>
+    <div class="btn">1</div>
+    <div class="btn">2</div>
+    <div class="btn">3</div>
+    <div class="btn operator">+</div>
+    <div class="btn zero">0</div>
+    <div class="btn">.</div>
+    <div class="btn operator">=</div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      current: "500"
+    };
+  },
+  methods: {
+    clear() {
+      this.current = "";
+    },
+    sign() {
+      this.current = this.current.charAt(0) === "-" ? this.current.slice(1) : `-${this.current}`;
+    },
+    percent() {
+      this.current = `${parseFloat(this.current) / 100}`;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  font-size: 40px;
+  grid-auto-rows: minmax(50px, auto);
+}
+.display {
+  grid-column: 1 / 5;
+  background-color: #333;
+  color: #eee;
+}
+.zero {
+  grid-column: 1 / 3;
+}
+.btn {
+  border: 1px solid #333;
+  background-color: #eee;
+}
+.operator {
+  background-color: orange;
+  color: #eee;
+}
+</style>
