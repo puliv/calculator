87bf518ea6d5d7782b9259d3cfb4f2e56a8c3ce2
puliv <pverapavez@gmail.com>
2021-01-09 21:36:53 -0300

    ok

---
+++ deploy.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+yarn run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+
+git push -f git@github.com:puliv/calculator.git main:gh-pages
+
+cd -
